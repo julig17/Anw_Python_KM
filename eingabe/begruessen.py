d14099524db744617fba4a5ccbd6616a6d7808f8
julig17 <greif.julia.jg@gmail.com>
2026-03-23 08:43:01 +0100

    Neues Skript aus Unterricht 23.03

---
+++ eingabe/begruessen.py
@@ -0,0 +1,10 @@
+'''
+Dieses Skript liest einen Nachnamen von der Standardeingabe ein
+speichert die Eingabe in die Variable nachname
+liest einen Vornamen von der Standardeingabe ein
+speichert die Eingabe in die Variable vorname
+und gibt die personalisierte Begrüßung aus
+'''
+nachname = input("Gebe einen Nachnamen ein:")
+vorname = input("Gebe einen Vornamen ein:")
+print("Hallo", vorname, nachname)
