8655b2a840ab74f3618ef42469d09933c322ca82
julig17 <greif.julia.jg@gmail.com>
2026-04-15 15:39:58 +0200

    Globale Variable

---
+++ sprachelemente/globale_variable.py
@@ -0,0 +1,15 @@
+#Zugriff auf eine globale Variable innerhalb einer Funktion
+#In diesem Beispiel wird eine globale Variable definiert und innerhalb einer Funktion auf sie zugegriffen.
+#Definiert man innerhalb der Funktion eine Variable mit demselben Namen und anderem Wert, 
+#wird die globale Variable innerhalb der Funktion überschattet und es entsteht eine lokale Variable.
+#Wert der Globalen Variable bleibt unverändert
+def funktion_ausgabe():
+    #Wert der globalen Variable wird ausgegeben
+    globale_variable = "Ich bin eine lokale Variable"
+    print("In Funktion: ",globale_variable)
+
+#Definition einer globalen Variable
+globale_variable = "Ich bin eine globale Variable"
+#Aufruf der Funktion, die auf die globale Variable zugreift
+funktion_ausgabe()
+print("Außerhalb Funktion: ",globale_variable)
