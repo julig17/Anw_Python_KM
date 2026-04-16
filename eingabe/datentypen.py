8a3a733f764e05de1457a865b60625eb52d04dd6
julig17 <greif.julia.jg@gmail.com>
2026-04-16 09:09:34 +0200

    Konvertierung von Datentypen

---
+++ eingabe/datentypen.py
@@ -0,0 +1,9 @@
+
+inhalt_eingabe = int(input("Gib etwas über Konsole ein:"))
+print(inhalt_eingabe)
+print(type(inhalt_eingabe))
+
+zahl = 2
+summe = zahl + inhalt_eingabe
+print(summe)
+print(type(summe))
