f5b660744be0c1fc008706d88e7f97d62c8b6076
julig17 <greif.julia.jg@gmail.com>
2026-04-22 11:16:42 +0200

    vorbereitung

---
+++ sprachelemente/bool.py
@@ -0,0 +1,17 @@
+x = True
+y = False
+# negierte Zuweisung
+a = not x
+print("a:",a)
+print(not y)
+# logische Operation and
+c = x and y
+print("c:",c)
+# logische Operation or
+d = x or y
+print("d",d)
+zahl = 1
+print(not zahl)
+zahl2 = 0
+print(not zahl2)
+print(not 'G')
---
+++ sprachelemente/operatoren.py
@@ -0,0 +1,24 @@
+int_zahl = 2
+int_zahl2 = 4
+
+print("Summe: ", int_zahl+int_zahl2)
+print("Produkt: ", int_zahl*int_zahl2)
+print("Potenz: ", int_zahl**3)
+print("Modulo:", 30%7)
+print("Ganzzahl:", 30//7)
+print("Vergleich:", int_zahl > int_zahl2)
+print("Vergleich:", int_zahl < int_zahl2)
+
+float_zahl = 2.0
+float_zahl2 = 4.0
+
+print("Summe: ", float_zahl+float_zahl2)
+print("Produkt: ", float_zahl*float_zahl2)
+print("Potenz: ", float_zahl**3)
+
+text1 = "Hi"
+text2 = "Du und Ich"
+
+print("Summe: ", text1+text2)
+print("Produkt: ", text1*5)
+print("Vergleich:", text1 > text2)
