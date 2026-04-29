ede35e11c4474ee7c73c815eb4b19854e8e1669e
julig17 <greif.julia.jg@gmail.com>
2026-04-29 11:25:46 +0200

    Bedingte Anweisung

---
+++ verzweigungen/bedingte_anweisung.py
@@ -0,0 +1,14 @@
+#einfache bedingte Anweisung
+
+zahl = 40
+#zahl = 41
+if(zahl %2 == 0):
+    print("Gerade")
+
+#einfache bedingte Anweisung mit mehreren Anweisungen richtig einrücken
+
+zahl = 40
+#zahl = 41
+if(zahl %2 == 0):
+    print("Gerade")
+    print("Das soll nur passieren wenn die Bedingung true ist")
