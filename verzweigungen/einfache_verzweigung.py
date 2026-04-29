aa23451a0b59640172a492b0672a8ce6227f50a3
julig17 <greif.julia.jg@gmail.com>
2026-04-29 11:55:01 +0200

    aufgaben

---
+++ verzweigungen/einfache_verzweigung.py
@@ -0,0 +1,9 @@
+
+#einfache verzweigungen, wenn ..dann
+
+zahl = 40
+#zahl = 41
+if(zahl %2 == 0):
+    print("Gerade")
+else:
+    print("Ungerade")
---
+++ verzweigungen/mehrere_verzweigungen.py
@@ -0,0 +1,12 @@
+alter = 66
+
+if alter < 13:
+  print("Kind")
+elif alter < 20:
+  print("Teenager")
+elif alter < 67:
+  print("Erwachsener")
+elif alter >= 67:
+  print("Rentner")
+
+
