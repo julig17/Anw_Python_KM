672c1dcd871ec1ba8eeab71e85bb0c90ceedb000
julig17 <greif.julia.jg@gmail.com>
2026-06-03 11:07:36 +0200

    Code vom Unterricht am 03.06

--- listen/unterricht.py
+++ listen/unterricht.py
@@ -1,60 +1,99 @@
-zahlen_liste = [1,2,5,15]
-print(zahlen_liste)
+# liste in python
 
-zahlen_liste_2 = [5,5,8,8]
-print(zahlen_liste_2)
+"""
 
-gemischte_liste = ["Hallo", 5, 5.5, "Welt"]
-print(gemischte_liste)
+print(type(automarken))
+autoreifen = [5,2]
 
-gemischte_liste_2 = ["Hallo", 5, 5.5, "Welt", [1,2,5]]
-print(gemischte_liste_2)
-print(gemischte_liste_2[-1])
-
-dictionary = {"name" : "Greif"}
-liste = [1,2, dictionary]
-print(liste)
+zahlen = [1,5,2,8,7]
+wort = "HAllo"
+gemischte_datentypen = ["BMW", 5, 2.7, [5,2], zahlen, wort, autoreifen]
 
 
-mehrdimensionale_array = [[1,2,3], [4,5,6]]
-print(mehrdimensionale_array)
-print(mehrdimensionale_array[0][1])
+zahlen[2]
+zahlen[2:-1:2]
+print(gemischte_datentypen[::-1])
 
-print(mehrdimensionale_array[1][2])
-
-slice_liste = [2,4,6,7,8,9]
-#vom anfang bis incl. 3.Element
-print(slice_liste[::-1])
-
-slice_liste[1] = 5
-print(slice_liste)
+zahlen_2 = [1,5,2,8,7]
+print(zahlen_2)
+print(zahlen_2[1])
+zahlen_2[1] = "HAllo"
+print(zahlen_2)
+"""
 
 
-zahlen_liste = [1,2,5,15]
-print(zahlen_liste)
+automarken = ["BMW", "Opel", "VW"]
+zahlen = [1,5,2,8,7]
+zahlen_2 = [1,5,2,8,7]
+print(automarken)
+print(zahlen)
+neue_liste = automarken+zahlen
+print(neue_liste)
+print(automarken+zahlen)
+print(automarken)
+print(zahlen)
 
-zahlen_liste_2 = [5,5,8,8]
-print(zahlen_liste_2)
+wort = "Hallo"
+#Achtung hier FEhler
+# print(zahlen * zahlen_2)
 
-print(zahlen_liste_2*3)
-
-zahlen_liste = [1,2,5,15]
-print(zahlen_liste)
-
-zeichenketten = ["Das", "ist", "in", "der", "Liste"]
-
-wort = "ist"
-if wort in zeichenketten:
-    print(wort)
-del zeichenketten[0:2]
-print(zeichenketten)
-if wort in zeichenketten:
-    print(wort)
-
-zeichenketten += ["noch", "das"]
-print(zeichenketten)
+# in und not in Operator
+print("a" in wort)
+print("k" in wort)
+print("k" not in wort)
+print("*" *20)
+print("BMW" in automarken)
+print("bmw" in automarken)
+print(5 in automarken)
+print("k" not in automarken)
+print("*" *20)
+# Achtung damit verändert mn die Liste im Unterschied zum + Operator
+del zahlen_2[0]
+print(zahlen_2)
 
 
+print("*" *20)
+print(zahlen)
+del zahlen[1:3]
+print(zahlen)
 
+zahl1 = 5
+zahl2 = 8
+#zahl2 += zahl1
+zahl2 = zahl2 + zahl1
+print(zahl2)
 
+zahlen = [1,5,2,8,7]
+zahlen_2 = [1,5,2,8,7]
+zahlen_2 += zahlen
+print(zahlen_2)
 
+print(len(zahlen_2))
+
+print("*" *20)
+for zaehler in range(2,10,2):
+    print(zaehler)
+
+print
+
+for buchstabe in "ZEichenkette":
+    print(buchstabe)
+
+#Zugriff auf alle elemente in der Liste
+# python beliebt Variante
+for element in automarken:
+    print(element)
+
+zahl = 0
+automarken[zahl]
+
+print("*" *20)
+print(len(automarken))
+for zaehler in range(len(automarken)):
+    print("Bei Zähler:", zaehler, "ist in der Liste drin:", automarken[zaehler])
+
+print("*" *20)
+i = 0
+while i < len(automarken):
+	print(automarken[i])
+	i = i + 1
