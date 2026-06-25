25f5c21ab60b34970fa00c2399fb16268b59a2a2
julig17 <greif.julia.jg@gmail.com>
2026-06-25 11:12:24 +0200

    Iterativer Alg.

---
+++ geschaeftsprozesse/kalkulation.py
@@ -0,0 +1,52 @@
+"""
+Dieses Modul bietet Hilfsfunktionen im Bereich Kalkulationen an
+"""
+
+def berechne_gesamtumsatz(umsaetze):
+    """ Methode um den Gesamtumsatz  zu berechen 
+    
+    args:
+        umsätze (list): Liste mit numerischen Zahlen
+    returns: 
+        float: Durchschnitt aller Zahlen
+    
+    """
+    summe = 0
+    if(len(umsaetze) == 0):
+        return -1
+    for umsatz in umsaetze:
+        summe +=umsatz
+    return summe
+
+def max_umsatz(umsaetze):
+    """ Methode um den Größten Umsatz zu bestimmen
+    
+    args:
+        umsätze (list): Liste mit numerischen Zahlen
+    returns: 
+        float: Größter Umsatz
+    
+    """
+    maximum = umsaetze[0]
+
+    for umsatz in umsaetze:
+        if umsatz > maximum:
+            maximum = umsatz
+
+    return maximum
+
+def rabatt_anwenden(*, umsaetze, rabatt):
+    """ Methode um auf alle Umsätze Rabatt anzuwenden
+    
+    args:
+        umsätze (list): Liste mit numerischen Zahlen
+    returns: 
+        list: Rabattierte Umsätze
+    
+    """
+    neue_preise = []
+
+    for umsatz in umsaetze:
+        neue_preise.append(umsatz * (1 - rabatt / 100))
+
+    return neue_preise
--- geschaeftsprozesse/test.py
+++ geschaeftsprozesse/test.py
@@ -1,4 +1,5 @@
 import kaufmaennisches_rechnen as kr
+import kalkulation as ka
 
 
 print(kr.berechne_durchschnitt.__doc__)
@@ -12,3 +13,12 @@ print(kr.berechne_prozentwert(grundwert=200, prozentsatz=25))
 print(kr.berechne_grundwert(prozentwert=1000, prozentsatz=1))
 print(kr.berechne_grundwert(prozentwert=200, prozentsatz=25))
 print(kr.berechne_prozentsatz(grundwert=200, prozentwert=20))
+
+
+umsaetze = [2500, 575.45, 222.99, 40.50, 10.39]
+gesamt_umsatz = ka.berechne_gesamtumsatz(umsaetze)
+print(f"GesamtUmsatz: {gesamt_umsatz:.2f} €")
+max_umsatz = ka.max_umsatz(umsaetze)
+print(f"Max Umsatz: {max_umsatz:.2f} €")
+rabattierte_umsaete = ka.rabatt_anwenden(umsaetze=umsaetze, rabatt=1)
+print(f"Rabattierte Umätze: {', '.join(f'{umsatz:.2f} €' for umsatz in rabattierte_umsaete)}")
