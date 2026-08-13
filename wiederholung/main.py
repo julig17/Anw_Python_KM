9da06280efbceb4b0246536682764c5cf9241f94
julig17 <greif.julia.jg@gmail.com>
2026-08-13 11:10:06 +0200

    Lösung mit Konstnten Schlüsseln

--- wiederholung/main.py
+++ wiederholung/main.py
@@ -1,3 +1,8 @@
+NUMMER = "nummer"
+BEZEICHNUNG = "bezeichnung"
+PREIS = "preis"
+BESTAND = "bestand"
+
 
 def lese_aus_datei(dateiname):
     inhalt = []
@@ -13,17 +18,41 @@ def lese_aus_datei(dateiname):
 def entferne_whitespaces_und_verarbeite_artikel(inhalt):
     for i in range(len(inhalt)):
         inhalt[i] = inhalt[i].strip().split(';') 
-        inhalt[i][0] = int(inhalt[i][2])  # Artikelnummer
-        inhalt[i][2] = float(inhalt[i][2])  # Preis
-        inhalt[i][3] = int(inhalt[i][3])    # Menge 
+        for j in range(len(inhalt[i])):
+            inhalt[i][j] = datentyp_konvertieren(inhalt[i][j])
+        #inhalt[i][0] = int(inhalt[i][0])  # Artikelnummer
+        #inhalt[i][2] = float(inhalt[i][2])  # Preis
+        #inhalt[i][3] = int(inhalt[i][3])    # Menge 
     return inhalt
 
+def datentyp_konvertieren(element):
+    try:
+        return int(element)
+    except ValueError:
+        try:
+            return float(element)
+        except ValueError:
+            return element
+
+def dump_in_dictionary(artikel_liste):
+    artikel_liste_dict = []
+    for artikel in artikel_liste:
+        artikel_dict = {
+            NUMMER: artikel[0],
+            BEZEICHNUNG: artikel[1],
+            PREIS: artikel[2],
+            BESTAND: artikel[3]
+        }
+        artikel_liste_dict.append(artikel_dict)
+    return artikel_liste_dict   
 
 datei_name = "./wiederholung/artikel.txt"
 inhalt = lese_aus_datei(datei_name)
 
 if len(inhalt) > 0:
     inhalt = entferne_whitespaces_und_verarbeite_artikel(inhalt)
-    print(inhalt)
+    artikel_liste_als_dict = dump_in_dictionary(inhalt)
+    print(artikel_liste_als_dict)
 else:
     print("Datei ist leer")
+
